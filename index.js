121a68c579dedc824aad74d24995cc3140bb29f7
Cornerstone <fortunecornerstone@gmail.com>
2021-04-29 22:42:39 +0100

    Update

--- index.js
+++ index.js
@@ -1,48 +1,90 @@
 function myPosts() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
     .then((response) => response.json())
-    .then((output) => {
-        // console.log(JSON)
-        document.getElementById('head1').innerHTML = "Posts";
-        document.getElementById('result1').innerHTML = JSON.stringify(output);
+    .then((data) => {
+        console.log(data)
+        let posts = '';
+        data.forEach(element => {
+            posts += `<div class="posts-list">
+            <ul>
+                <li>${element.body}</li>
+                <li>${element.id}</li>
+                <li>${element.title}</li>
+                <li>${element.userid}</li>
+            </ul>
+            </div>`
+        document.getElementById('head1').innerHTML = 'Posts';
+        document.getElementById('result1').innerHTML = posts;
+        });
     });
 };
 
 function myComments() {
-    fetch('https://jsonplaceholder.typicode.com/comments')
+    fetch('https://jsonplaceholder.typicode.com/comments/1')
     .then((response) => response.json())
-    .then((output) => {
-        // console.log(JSON)
+    .then((data) => {
+        console.log(data)
+        let comments = '';
+        data.forEach(element => {
+            comments += `<div class="comments-list">
+            <ul>
+                <li>${element.body}</li>
+                <li>${element.email}</li>
+                <li>${element.id}</li>
+                <li>${element.name}</li>
+                <li>${element.postid}</li>
+            </ul>
+            </div>`
         document.getElementById('head2').innerHTML = 'Comments';
-        document.getElementById('result2').innerHTML = JSON.stringify(output);
+        document.getElementById('result2').innerHTML = comments;
+        });
     });
 };
 
 function myAlbums() {
-    fetch('https://jsonplaceholder.typicode.com/albums')
+    fetch('https://jsonplaceholder.typicode.com/albums/1')
     .then((response) => response.json())
-    .then((ouput) => {
-        // console.log(JSON)
+    .then((data) => {
+        let albums = '';
+        data.forEach(element => {
+            albums += `<div class="album-list">
+            <ul>
+                <li>${element.id}</li>
+                <li>${element.title}</li>
+                <li>${element.userid}</li>
+            </ul>
+            </div>`
         document.getElementById('head3').innerHTML = 'Albums';
-        document.getElementById('result3').innerHTML = JSON.stringify(ouput);
+        document.getElementById('result3').innerHTML = albums;
+        });
     });
 };
 
 function myPhotos() {
-    fetch('https://jsonplaceholder.typicode.com/photos')
+    fetch('https://jsonplaceholder.typicode.com/photos/1')
     .then((response) => response.json())
     .then((data) => {
         console.log(data)
+        let photos = '';
+        data.forEach(element => {
+            photos += `<div class="todo-list">
+            <ul>
+                <li>${element.completed}</li>
+                <li>${element.id}</li>
+                <li>${element.title}</li>
+                <li>${element.userid}</li>
+            </ul>
+            </div>`
         document.getElementById('head4').innerHTML = 'Photos';
-        document.getElementById('result4').innerHTML = JSON.stringify(data);
+        document.getElementById('result4').innerHTML = photos;
+        });
     });
 };
 
 function myTodos() {
-    fetch('https://jsonplaceholder.typicode.com/todos')
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then((response) => response.json())
     .then((data) => {
-        console.log(data)
         let todos = '';
         data.forEach(element => {
             todos += `<div class="todo-list">
@@ -60,7 +102,7 @@ function myTodos() {
 };
 
 function myUsers() {
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch('https://jsonplaceholder.typicode.com/users/1')
     .then(response => response.json())
     .then(data => {
        let users = '';
